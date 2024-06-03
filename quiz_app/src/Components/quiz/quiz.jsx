0b97d55a81054259e8564bc6b875ae7d7bba10b5
David Ramovich Mandal <davidramovichmandal@gmail.com>
2024-06-03 15:54:31 +0545

    added the data files

--- quiz_app/src/Components/quiz/quiz.jsx
+++ quiz_app/src/Components/quiz/quiz.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './quiz.css'
 
 const Quiz = () => {
+
+
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
-        <h2>Which device is used for the internet connection ?</h2>
+        <h2>1. Which device is used for the internet connection ?</h2>
         <ul>
             <li>Modem</li>
             <li>Router</li>
